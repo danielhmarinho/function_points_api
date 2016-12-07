c9daa5e5f66904e330881bdb1d0d0d996b80a5b0
Daniel Henrique <danielhmarinho@gmail.com>
2016-12-07 15:24:48 -0200

    First class and tests

---
+++ db/migrate/20161207172139_create_organizations.rb
@@ -0,0 +1,11 @@
+class CreateOrganizations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.string :address
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
